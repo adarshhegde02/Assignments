953a93b3c1749cff203e758c45c74c8780133d27
adarshhegde02 <adarsh.hegde@thoughtfocus.com>
2021-12-22 20:16:19 +0530

    Exceptions Assignment

---
+++ exceptions/ArithmeticException.java
@@ -0,0 +1,7 @@
+package com.thought_focus.Morning.exception;
+
+public class ArithmeticException {
+	public static void main(String[] args) {
+		System.out.println(10 / 0);
+	}
+}
---
+++ exceptions/ArrayIndex.java
@@ -0,0 +1,12 @@
+package com.thought_focus.Morning.exception;
+
+public class ArrayIndex {
+	public static void main(String[] args) {
+		String cars[] = { "Alto", "Ciaz", "Baleno", "Swift" };
+
+		for (int i = 0; i <= cars.length; i++) {
+			System.out.println(cars[i]);
+		}
+
+	}
+}
