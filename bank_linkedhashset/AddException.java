5ce9fb00babebf457e35a55578d57838358cf6cc
Adarsh Hegde <96477669+adarshhegde02@users.noreply.github.com>
2021-12-29 20:40:14 +0530

    Delete bank_linkedhashset directory

--- bank_linkedhashset/AddException.java
+++
@@ -1,8 +0,0 @@
-package bank_linkedhashset.com.thoughtfocus.bank_linkedhashset;
-
-public class AddException extends RuntimeException {
-	@Override
-	public String getMessage() {
-		return "Not added...";
-	}
-}
